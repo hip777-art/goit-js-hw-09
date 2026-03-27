a94f61e62518de9983ba8e6a2af3c457867e911c
arkhipkaaa <arkhip24032000@gmail.com>
2026-03-27 13:04:55 +0200

    init

---
+++ src/js/1-gallery.js
@@ -0,0 +1,54 @@
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+window.global = window;
+
+const images = [
+  {
+    preview:
+      'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?w=300',
+    original:
+      'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?auto=format&fit=crop&w=1200&q=80',
+    description: 'Mountain',
+  },
+  {
+    preview:
+      'https://images.unsplash.com/photo-1492724441997-5dc865305da7?w=300',
+    original:
+      'https://images.unsplash.com/photo-1492724441997-5dc865305da7?auto=format&fit=crop&w=1200&q=80',
+    description: 'Lake',
+  },
+  {
+    preview:
+      'https://images.unsplash.com/photo-1470770841072-f978cf4d019e?w=300',
+    original:
+      'https://images.unsplash.com/photo-1470770841072-f978cf4d019e?auto=format&fit=crop&w=1200&q=80',
+    description: 'River',
+  },
+];
+
+const gallery = document.querySelector('.gallery');
+
+const galleryMarkup = images
+  .map(
+    ({ preview, original, description }) => `
+      <li class="gallery-item">
+        <a class="gallery-link" href="${original}">
+          <img
+            class="gallery-image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `
+  )
+  .join('');
+
+gallery.innerHTML = galleryMarkup;
+
+const { default: SimpleLightbox } = await import('simplelightbox');
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
